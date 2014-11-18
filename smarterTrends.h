f57b08475f94c2e403e9c97e9a12662b68bdae29
unknown <wilso199@EAS-BEN006-C6.it.muohio.edu>
2014-11-18 12:45:26 -0500

    Fixed isSorted issue

--- smarterTrends.h
+++ smarterTrends.h
@@ -2,9 +2,12 @@
 
 class smarterTrends : public naiveTrends {
 public:
+	smarterTrends(){
+		isSorted = false;
+	}
 	virtual void increaseCount(std::string s, unsigned int amount);
 	virtual std::string getNthPopular(unsigned int n);
 
 protected:
-	bool isSorted = false;
+	bool isSorted;
 };
